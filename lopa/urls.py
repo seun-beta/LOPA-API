f9fcc36da06c05a3e833979f9977055ed9c14ef7
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-08-18 08:57:45 +0100

    add id for filter

--- lopa/urls.py
+++ lopa/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = "lopa"
+
 urlpatterns = [
-    path('cause', views.CauseData.as_view()),
-    path('cause_barrier', views.CauseBarrierData.as_view()),
-    path('consequence', views.ConsequenceData.as_view()),
-    path('consequence_barrier', views.ConsequenceBarrierData.as_view()),
-    path('event', views.EventData.as_view()),
+    path('cause/<int:cause_id>', views.CauseData.as_view()),
+    path('cause_barrier/<int:cause_barrier_id>', views.CauseBarrierData.as_view()),
+    path('consequence/<int:consequence_id>', views.ConsequenceData.as_view()),
+    path('consequence_barrier/<int:pk>', views.ConsequenceBarrierData.as_view()),
+    path('event/<int:event_id>', views.EventData.as_view()),
 ]

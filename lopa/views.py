69da3c35cb559346bef612184986c1dd263dae83
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-08-18 08:57:02 +0100

    add index view

--- lopa/views.py
+++ lopa/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import View
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
@@ -10,9 +11,22 @@ from .serializers import CauseSerializer, CauseBarrierSerializer, \
 from lopa import serializers
 
 
-class CauseData(APIView):
+class IndexView(View):
+    def get(self, request):
+        event_list = Event.objects.all()
+        ctx = {'event_list': event_list}
+        return render(request, 'lopa/index.html', ctx)
+
+
+class DataView(View):
 
     def get(self, request):
+        pass
+
+
+class CauseData(APIView):
+
+    def get(self, request, cause_id):
         cause = Cause.objects.all().values("cause_id", "description", "initial_frequency", "event_id", "target_frequency")
         cause_list = list(cause)
         data = cause_list 
@@ -21,7 +35,7 @@ class CauseData(APIView):
 
 class CauseBarrierData(APIView):
     
-    def get(self, request):
+    def get(self, request, cause_barrier_id):
         cause_barrier = CauseBarrier.objects.all().values("cause_barrier_id", "description", "pfd", "cause_id")
         cause_barrier_list = list(cause_barrier)
         data = cause_barrier_list 
@@ -31,7 +45,7 @@ class CauseBarrierData(APIView):
 
 class ConsequenceData(APIView):
     
-    def get(self, request):
+    def get(self, request, consequence_id):
         consequence = Consequence.objects.all().values("consequence_id", "description", "initial_frequency", "target_frequency")
         consequence_list = list(consequence)
         data = consequence_list 
@@ -41,16 +55,14 @@ class ConsequenceData(APIView):
 
 class ConsequenceBarrierData(APIView):
     
-    def get(self, request):
-        consequence_barrier = ConsequenceBarrier.objects.all().values("consequence_barrier_id", "description", "pfd", "consequence_id")
-        consequence_barrier_list = list(consequence_barrier)
-        data = consequence_barrier_list 
-        new_data = ConsequenceBarrierSerializer(data, many=True)
+    def get(self, request, consequence_barrier_id):
+        consequence_barrier = ConsequenceBarrier.objects.get(pk=consequence_barrier_id)
+        new_data = ConsequenceBarrierSerializer(consequence_barrier, many=True)
         return Response(new_data.data)
 
 class EventData(APIView):
     
-    def get(self, request):
+    def get(self, request, event_id):
         event = Event.objects.all().values("event_id","description", "cause_id", "consequence_id")
         event_list = list(event)
         data = event_list 

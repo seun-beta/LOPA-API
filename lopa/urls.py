95c4816bbbdd20d51fea18354548474e1093dd31
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-08-21 14:39:44 +0100

    add new url for api views

--- lopa/urls.py
+++ lopa/urls.py
@@ -5,9 +5,10 @@ from . import views
 app_name = "lopa"
 
 urlpatterns = [
-    path('cause/<int:cause_id>', views.CauseData.as_view()),
-    path('cause_barrier/<int:cause_barrier_id>', views.CauseBarrierData.as_view()),
-    path('consequence/<int:consequence_id>', views.ConsequenceData.as_view()),
-    path('consequence_barrier/<int:pk>', views.ConsequenceBarrierData.as_view()),
-    path('event/<int:event_id>', views.EventData.as_view()),
+    path("",views.EventList.as_view()),
+    path('cause/<cause_id>', views.CauseData.as_view()),
+    path('cause_barrier/<cause_barrier_id>', views.CauseBarrierData.as_view()),
+    path('consequence/<consequence_id>', views.ConsequenceData.as_view()),
+    path('consequence_barrier/<consequence_barrier_id>', views.ConsequenceBarrierData.as_view()),
+    path('event/<event_id>', views.EventData.as_view()),
 ]
